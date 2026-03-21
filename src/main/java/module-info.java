1e5d81700ebeea1d974d4cce7c57cc52c419fa6c
Lilly Schüpferling <lil.schuepferling.21@lehre.mosbach.dhbw.de>
2026-03-22 00:10:19 +0100

    add requirements (scr/main/java/module-info.java)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
-module com.example.intfrontend {
+module com.example.controllers {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires com.fasterxml.jackson.databind;
 
-
-    opens com.example.intfrontend to javafx.fxml;
-    exports com.example.intfrontend;
+    opens com.example.controllers to javafx.fxml;
+    opens models to com.fasterxml.jackson.databind;
+    exports com.example.controllers;
+    exports models;
 }
