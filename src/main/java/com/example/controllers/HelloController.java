3c9e0b7672ee5b5b4b0ff97082f9ab86df4c8db5
Lilly Schüpferling <lil.schuepferling.21@lehre.mosbach.dhbw.de>
2026-03-22 20:16:44 +0100

    add tutorialId, openUpdateView(), getTutorialById(), OpenCreateView() and edit initialize() (src/main/java/com/example/controllers/HelloController.java)

--- src/main/java/com/example/controllers/HelloController.java
+++ src/main/java/com/example/controllers/HelloController.java
@@ -1,23 +1,24 @@
 package com.example.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Callback;
 import models.Tutorial;
 import services.TutorialService;
 
-import java.net.URI;
+import java.io.IOException;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -26,6 +27,9 @@ public class HelloController implements Initializable {
 
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private TextField tutorialId;
 
     @FXML
     private TableView<Tutorial> tutorialTable;
@@ -46,20 +50,19 @@ public class HelloController implements Initializable {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
 
         try {
 
             List<Tutorial> tutorials = tutorialService.getAllTutorials();
 
-            ObservableList<Tutorial> hardwareList =
+            ObservableList<Tutorial> tutorialList =
                     FXCollections.observableArrayList(tutorials);
 
-            tutorialTable.setItems(hardwareList);
+            tutorialTable.setItems(tutorialList);
 
         } catch (Exception e) {
             e.printStackTrace();
-            welcomeText.setText("Error fetching hardware data!");
+            welcomeText.setText("Error fetching tutorial data!");
         }
     }
 
@@ -67,7 +70,94 @@ public class HelloController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
-        descColumn.setCellValueFactory(new PropertyValueFactory<>("desc"));
-        publColumn.setCellValueFactory(new PropertyValueFactory<>("publ"));
+        descColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        publColumn.setCellValueFactory(new PropertyValueFactory<>("published"));
+
+        tutorialTable.setRowFactory(
+                new Callback<TableView<Tutorial>, TableRow<Tutorial>>() {
+                    @Override
+                    public TableRow<Tutorial> call(TableView<Tutorial> tableView) {
+                        final TableRow<Tutorial> row = new TableRow<>();
+                        final ContextMenu rowMenu = new ContextMenu();
+                        MenuItem editItem = new MenuItem("Edit");
+                        editItem.setOnAction(new EventHandler<ActionEvent>() {
+                            @Override
+                            public void handle(ActionEvent event) {
+                                Tutorial tutorial = row.getItem();
+                                openUpdateView(tutorial);
+                            }
+                        });
+                        MenuItem removeItem = new MenuItem("Delete");
+                        removeItem.setOnAction(new EventHandler<ActionEvent>() {
+
+                            @Override
+                            public void handle(ActionEvent event) {
+                                try {
+                                    Tutorial tutorial = row.getItem();
+                                    tutorialTable.getItems().remove(tutorial);
+                                    tutorialService.delete(tutorial);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    //alert
+                                }
+                            }
+                        });
+                        rowMenu.getItems().addAll(editItem, removeItem);
+
+                        // only display context menu for non-empty rows:
+                        row.contextMenuProperty().bind(
+                                Bindings.when(row.emptyProperty())
+                                        .then((ContextMenu) null)
+                                        .otherwise(rowMenu));
+                        return row;
+                    }
+                });
+
+
     }
+
+    private void openUpdateView(Tutorial tutorial) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(HelloApplication.class.getResource("/com/example/views/updateTutorial.fxml"));
+        try {
+            Parent parent = (AnchorPane) loader.load();
+            Scene scene = new Scene(parent);
+            HelloApplication.getCentralStage().setScene(scene);
+            PutTutorialController controller = loader.getController();
+            controller.setTutorial(tutorial);
+            HelloApplication.getCentralStage().show();
+            controller.fillTutorialFields();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    protected void getTutorialById(){
+        String input = tutorialId.getText();
+
+        try {
+            long id = Long.parseLong(input);
+
+            List<Tutorial> result = tutorialService.getById(Long.parseLong(String.valueOf(id)));
+
+            ObservableList<Tutorial> tutorialList =
+                    FXCollections.observableArrayList(result);
+
+            tutorialTable.setItems(tutorialList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void OpenCreateView() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/views/createTutorial.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        HelloApplication.getCentralStage().setTitle("Create");
+        HelloApplication.getCentralStage().setScene(scene);
+        HelloApplication.getCentralStage().show();
+    }
+
 }
